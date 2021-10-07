edda824e5a88c48bf86794ad23fee81bfd9f7e3f
Cavid <mehsumovcavid@gmail.com>
2021-10-07 19:29:03 +0400

    Add project files.

---
+++ Ado.Net Task1/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ado.Net_Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+        string cs = "";
+        DataTable table;
+        SqlDataReader reader;
+        DataSet dataSet;
+        SqlDataAdapter dataadapter;
+        DataRowView DataRowView;
+        string ProductName;
+        string ProductId;
+        string price;
+        string Description;
+        int count;
+        int count1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            TableCombobox.Items.Add("Products");
+            TableCombobox.Items.Add("Orders");
+            TableCombobox.Items.Add("Customers");
+            TableCombobox.Items.Add("OrderDetailes");
+        }
+        public void RefreshTable()
+        {
+
+            using (conn = new SqlConnection())
+            {
+                MyGrid.ItemsSource = null;
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(dataadapter);
+                dataadapter.Fill(dataSet);
+
+                MyGrid.ItemsSource = dataSet.Tables[0].DefaultView;
+
+                sqlCommandBuilder.GetUpdateCommand();
+            }
+        }
+
+        private void TableCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                dataSet = new DataSet();
+                dataadapter = new SqlDataAdapter($@"select *from {TableCombobox.SelectedItem.ToString()}", conn);
+
+
+                RefreshTable();
+
+            }
+        }
+
+        private void MyGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataRowView = MyGrid.SelectedItem as DataRowView;
+
+            try
+            {
+                if (DataRowView != null)
+                {
+                    ProductId = DataRowView["Id"].ToString();
+                    ProductName = DataRowView["Name"].ToString();
+                    price = DataRowView["Price"].ToString();
+                    Description = DataRowView["Description"].ToString();
+
+                }
+
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                ++count;
+                SqlCommand command = new SqlCommand("sp_InsertOrder", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                var param1 = new SqlParameter();
+                param1.Value = count;
+                param1.ParameterName = "@OrderId";
+                param1.SqlDbType = SqlDbType.Int;
+                command.Parameters.Add(param1);
+
+
+                var param2 = new SqlParameter();
+                param2.Value = int.Parse(ProductId);
+                param2.ParameterName = "@ProductiD";
+                param2.SqlDbType = SqlDbType.Int;
+                command.Parameters.Add(param2);
+
+
+                var param3 = new SqlParameter();
+                param3.Value = 1;
+                param3.ParameterName = "@CustomerId";
+                param3.SqlDbType = SqlDbType.Int;
+                command.Parameters.Add(param3);
+
+
+                command.ExecuteNonQuery();
+                ++count1;
+
+                SqlCommand command1 = new SqlCommand("sp_InsertOrderDetails", conn);
+                command1.CommandType = CommandType.StoredProcedure;
+                var c1p = new SqlParameter();
+                c1p.Value = count1;
+                c1p.ParameterName = "@OrderId";
+                c1p.SqlDbType = SqlDbType.Int;
+                command1.Parameters.Add(c1p);
+
+
+                var c2p = new SqlParameter();
+                c2p.Value = ProductName;
+                c2p.ParameterName = @"ProductName";
+                c2p.SqlDbType = SqlDbType.NVarChar;
+                command1.Parameters.Add(c2p);
+
+                var c3p = new SqlParameter();
+                c3p.Value = price;
+                c3p.ParameterName = "@ProductPrice";
+                c3p.SqlDbType = SqlDbType.NVarChar;
+                command1.Parameters.Add(c3p);
+
+                command1.ExecuteNonQuery();
+
+            }
+
+            // RefreshTable();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                
+                SqlCommand command = new SqlCommand("sp_DeleteProduct", conn);
+                command.CommandType = CommandType.StoredProcedure;
+
+                var param1 = new SqlParameter();
+                param1.Value = ProductId;
+                param1.ParameterName = "@ProductId";
+                param1.SqlDbType = SqlDbType.Int;
+                command.Parameters.Add(param1);
+
+                command.ExecuteNonQuery(); 
+
+                RefreshTable();
+            }
+        }
+    }
+}
